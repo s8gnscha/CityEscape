73ccf2b554efbe1227c7f0e65e237d1c836a35ed
s8gnscha <s8gnscha@stud.uni-saarland.de>
2021-12-10 21:11:23 +0100

    Dokumentieren von sachen

--- CityEscape/Assets/Scripts/CameraMove.cs
+++ CityEscape/Assets/Scripts/CameraMove.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
-
+/**
+ * Skript, der die Kamera bewegt. Die Kamera soll sich automatisch mit dem Spieler bewegen
+ */
 public class CameraMove : MonoBehaviour
 {
+    //Position des Spielers
    public Transform target;
   
+   //Wie flüssig die Kamera gehen soll
    public float smoothSpeed = 0.125f;
+   //der wert, wie viel die kamera eigentlich vom spieler verschoben werden soll
    public Vector3 offset;
   
+   /**
+    * Bei FixedUpdate wird die Position der Kamera mithilfe von offset und target ausgerichtet.
+    * Sie werden zusammenaddiert und mithilfe von smoothedPosition angepasst, damit die Position
+    * der Kamera ständig automatisch nach dem Spieler (zsm. mithilfe von lookat) ausgerichtet ist
+    */
    void FixedUpdate ()
    {
     Vector3 desiredPosition = target.position + offset;
--- CityEscape/Assets/Scripts/PlayerMove.cs
+++ CityEscape/Assets/Scripts/PlayerMove.cs
@@ -1,10 +1,16 @@
 
 using UnityEngine;
 
+
+/**
+ * Movement vom Player. Beschränkt sich zunächst nur auf laufen
+ */
 public class PlayerMove : MonoBehaviour
 {
+	
+	//Rigidbody vom Spieler
    public Rigidbody rb;
-
+	//Position des Spielers
  	public Transform trans;
    
      
@@ -15,9 +21,14 @@ public class PlayerMove : MonoBehaviour
 			
        }
    
+       
+       /**
+        * Bei fixedupdate wird durch die Velocity function vom rigidbody das automatische laufen vom Spieler generiert.
+        * Die Auskommentieren sind weitere Ansätze die bisher noch nicht funktionieren ^^"
+        */
        void FixedUpdate()
        {
-				/*float speed=5f;
+				float speed=5f;
 				rb.velocity=new Vector3(0,0,-speed);
                   //rb.AddForce(0, 0, -700 * Time.deltaTime);
 					/*Vector3 pos = transform.position;
